19b8ca21c59bfb0f8c508852a4b22ad01493dc6e
Hugo Alejandro Gomez Herrera <hugomez@Hugos-MacBook-Air.local>
2023-02-17 08:55:35 -0600

    second commit

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,32 @@
+// estamos usando .NET
+// aqui importamos namespaces
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+
+    //ciclo de vida / lifecycle
+    // existen metodos que se invocan en momentos especificos de la vida del script
+    void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate(){}
+
+    void LateUpdate(){}
+}
